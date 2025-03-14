5233e8a90b1b4130cf48d66ed9523a95b36c4477
Evan Thomas <evan.thomas@epfl.ch>
2025-03-14 15:06:11 +0100

    Colour login button

--- src/common/views.rs
+++ src/common/views.rs
@@ -2,19 +2,23 @@ use crate::common::models::{AppState, Message};
 use iced::widget::{button, column, container, horizontal_space, row, text};
 use iced::{Element, Fill};
 
+// struct ButtonStyle;
+
+// impl ButtonStyle {
+//     const GREEN: iced::button::Style = iced::button::Style {
+//         background: Some(iced::Color::from_rgb8(0x00, 0x80, 0x00)),
+//         border_radius: 5.0,
+//         text_color: iced::Color::WHITE,
+//         ..iced::button::Style::default()
+//     };
+// }
+
 pub fn view_main(state: &AppState) -> Element<Message> {
     container(
         column![
             row![
                 horizontal_space(),
                 text("Welcome to the AstroRiver toolbox").size(20),
-                if !state.sensors.is_empty() {
-                    Element::<Message>::from(
-                        button("View Sensors").on_press(Message::ShowSensorsPage),
-                    )
-                } else {
-                    Element::<Message>::from(button("View Sensors"))
-                },
                 horizontal_space(),
             ]
             .spacing(10),
@@ -39,13 +43,40 @@ pub fn view_main(state: &AppState) -> Element<Message> {
                         .as_ref()
                         .map(|claims| claims.email.as_str())
                         .unwrap_or("Logout");
-                    button(user_logged_in_identifier).on_press(Message::Logout)
+                    button(user_logged_in_identifier)
+                        .on_press(Message::Logout)
+                        .style(|_, _| {
+                            iced::widget::button::Style {
+                                background: Some(iced::Color::from_rgb8(0x00, 0x80, 0x00).into()), // Green
+                                text_color: iced::Color::WHITE,
+                                border: iced::Border::default(),
+                                shadow: iced::Shadow::default(),
+                            }
+                        })
                 } else {
-                    button("Login").on_press(Message::Login)
+                    button("Login").on_press(Message::Login).style(|_, _| {
+                        iced::widget::button::Style {
+                            background: Some(iced::Color::from_rgb8(0xFF, 0x00, 0x00).into()), // Red
+                            text_color: iced::Color::WHITE,
+                            border: iced::Border::default(),
+                            shadow: iced::Shadow::default(),
+                        }
+                    })
                 },
                 horizontal_space(),
             ]
             .spacing(10),
+            row![
+                horizontal_space(),
+                if !state.sensors.is_empty() {
+                    Element::<Message>::from(
+                        button("View Sensors").on_press(Message::ShowSensorsPage),
+                    )
+                } else {
+                    Element::<Message>::from(button("View Sensors"))
+                },
+                horizontal_space(),
+            ]
         ]
         .spacing(25),
     )
